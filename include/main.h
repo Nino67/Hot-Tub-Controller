e00b6f03d0e51324935b0448a2a644027d4e1be4
Gaetano (Nino) Ricca <gricca1967@gmail.com>
2025-03-06 01:45:56 -0500

    added and tested json serialzation function, working

--- include/main.h
+++ include/main.h
@@ -1,8 +1,11 @@
-#include <Arduino.h>
 #include "globals.h"
-#include "ArduinoJson-v6.19.4.h" 
-#include "pid.h"
 #include <SPI.h>
+#include <max6675.h>
+#include <ArduinoJson.h>
+#include <pid.h>
+
+// #include <SPI.h>
+// #include <ArdunoJson.h>
 
 
 #define BAUDRATE 57600
@@ -16,3 +19,21 @@
 #define START_UP_DELAY 3500
 #define READ_TIME_DELAY 500
 #define STATUS_UPDATE_TIME 200
+
+// Buffer Size for UART
+#define MAX_BUFFER_SIZE 200
+
+// Hot Tub Structure Definition
+static struct HotTubStruct {
+  // Hot Tub Variables
+  float currentTemp = 20;
+  float setTemp = 20;
+  float airTemp = 0;
+  bool HeaterOn = false;
+  bool pumpLowSpeedOn = false;
+  bool pumpHighSpeedOn = false;
+  // Variable to signify temperature went below setpoint
+  bool belowSetpoint = true;
+  // Variable to signify temperature range window below setpoint
+  int hysterisis = HYSTERISIS;
+} hotTub;
--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,64 @@
 #include "main.h"
-#include <SPI.h>
-#include <max6675.h>
 
-// put function declarations here:
+// function declarations 
+void setup();
+void loop();
 void toggleLED();
-float readThermocouple1();
-float readThermocouple2();
+void sendHotTubStatus();
 
-  // Define PID I/O Variables we'll be connecting to
-  double Setpoint, Input, Output;
+// Define PID I/O Variables we'll be connecting to
+double Setpoint, Input, Output;
 
-  // Specify the links and initial PID tuning parameters
-  double Kp = 2, Ki = 5, Kd = 1;
-  PID temperaturePID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+// Specify the links and initial PID tuning parameters
+double Kp = 2, Ki = 5, Kd = 1;
+PID temperaturePID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
-  // Define the pin numbers for the MAX6675
-  const int thermo1_CS = PB12;
-  const int thermo_CLK = PB13;
-  const int thermo_SO = PB14;
-  const int thermo2_CS = PB15;
+// Define the pin numbers for the MAX6675
+// const int thermocouple1_CS = PB12;
+// const int thermocouple_CLK = PB13;
+// const int thermocouple_SO = PB14;
+// const int thermocouple2_CS = PB15;
 
-  // Create an instance of the MAX6675 library
-  MAX6675 thermocouple1(thermo_CLK, thermo1_CS, thermo_SO);
-  MAX6675 thermocouple2(thermo_CLK, thermo2_CS, thermo_SO);
+// Create an instance of the MAX6675 library
+MAX6675 thermocouple1(THERMO_CLK_Pin, THERMO_CS_1_Pin, THERMO_SO_Pin);
+MAX6675 thermocouple2(THERMO_CLK_Pin, THERMO_CS_2_Pin, THERMO_SO_Pin);
 
-  // Define Serial1 manually (Serial2 is defined in the Arduino framework)
-  HardwareSerial Serial1(PA10, PA9); // RX, TX for USART1
-  // HardwareSerial Serial2(PA3, PA2); // RX, TX for USART2
+// Define Serial1 manually (Serial2 is defined in the Arduino framework)
+HardwareSerial Serial1(PA10, PA9); // RX, TX for USART1
+// HardwareSerial Serial2(PA3, PA2); // RX, TX for USART2
 
-  // Initialize Main loop timing variables
-  static uint32_t currentTime = 0;
-  static uint32_t lastStatusUpdate = 0;
-  static uint32_t lastReadUpdate = 0;
+// Initialize Main loop timing variables
+static uint32_t currentTime = 0;
+static uint32_t lastStatusUpdate = 0;
+static uint32_t lastReadUpdate = 0;
 
-  void setup()
-  {
-    // Initialize the LED pin as an output
-    pinMode(LED_Pin, OUTPUT);
+void setup()
+{
+  // Initialize the LED pin as an output
+  pinMode(LED_Pin, OUTPUT);
 
-    // Initialize SPI2
-    SPI.begin();
+  // Initialize SPI2
+  SPI.begin();
 
-    // Initialize UART1
-    Serial1.begin(9600); // Set the baud rate as needed
+  // Initialize UART1
+  Serial1.begin(9600); // Set the baud rate as needed
 
-    // Initialize UART2
-    Serial2.begin(9600); // Set the baud rate as needed
+  // Initialize UART2
+  Serial2.begin(9600); // Set the baud rate as needed
 
-    // Initialize USB serial communication for debugging
-    Serial.begin(9600);
+  // Initialize USB serial communication for debugging
+  Serial.begin(9600);
 
-    // Wait for MAX6675 to stabilize
-    delay(500);
+  // Wait for MAX6675 to stabilize
+  delay(500);
 
-    // Test message
-    Serial1.println("Hello from UART1");
+  // Test message
+  Serial1.println("Hello from UART1");
 
-    // Reset Main loop timing variables
-    currentTime = millis();
-    lastStatusUpdate = currentTime;
-    lastReadUpdate = lastStatusUpdate;
+  // Reset Main loop timing variables
+  currentTime = millis();
+  lastStatusUpdate = currentTime;
+  lastReadUpdate = lastStatusUpdate;
   }
 
   void loop()
@@ -85,12 +84,15 @@ float readThermocouple2();
     if ((currentTime - lastReadUpdate) >= READ_TIME_DELAY)
     {
       // Read the temperature from the MAX6675
-      double temperature = thermocouple1.readFahrenheit();
+      hotTub.currentTemp = thermocouple1.readFahrenheit();
 
       // Print the temperature to the serial monitor
       Serial.print("Temperature: ");
-      Serial.print(temperature);
+      Serial.print(hotTub.currentTemp);
       Serial.println(" °F");
+      Serial.println("\n\n");
+
+      sendHotTubStatus();
 
       if (Serial1.available() > 0)
       {
@@ -130,4 +132,28 @@ float readThermocouple2();
     digitalWrite(LED_Pin, ledState); // Set the LED to the new state
   }
 
+  void sendHotTubStatus()
+  {
+    // Create a JSON object to store the status data
+    // StaticJsonDocument<200> doc;
+    JsonDocument doc;
+
+    // Add the temperature data to the JSON object
+    // doc["temperature"] = thermocouple1.readFahrenheit();
+
+
+    doc["temp"] = hotTub.currentTemp;
+    doc["sTemp"] = hotTub.setTemp;
+    doc["heat"] = hotTub.HeaterOn;
+    doc["pLo"] = hotTub.pumpLowSpeedOn;
+    doc["pHi"] = hotTub.pumpHighSpeedOn;
   
+  
+
+    // Serialize the JSON object to a string
+    String output;
+    serializeJson(doc, output);
+
+    // Transmit the JSON string over Serial
+    Serial.println(output);
+  }
